b56f390c83cd48df606f532aa35e0045dab7bcb6
moonlightboss <cumrak.legenda@gmail.com>
2023-03-26 16:31:35 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<weatherInfo></weatherInfo>
+</template>
+
+<script>
+import weatherInfo from './components/weatherInfo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    weatherInfo,
+  }
+}
+</script>
+
+<style>
+
+</style>
